d21cd59275fc55f371b5cb79f2bf3fc6e1537352
LuisAlvarado64 <laalvarado064@gmail.com>
2021-10-11 03:15:32 -0700

    sugoma

--- Agenda/src/agenda/Agenda.java
+++ Agenda/src/agenda/Agenda.java
@@ -128,7 +128,7 @@ public class Agenda {
         }
         System.out.println("Busqueda");
         String nombreBuscar="Nombre: Luis. Número: 6861519874. Cumpleaños: 2001-07-15";
-        for(int k;k<=contactos.size();k++){
+        for(int k=0;k<=contactos.size();k++){
         boolean existencia = contactos.contains(nombreBuscar);
         if(existencia){
             System.out.println("Existe");

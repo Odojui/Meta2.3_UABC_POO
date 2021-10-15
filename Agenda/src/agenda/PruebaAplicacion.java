e34eee2a499b492f6a521023dd0f209d96118abe
Odojui <49645850+Odojui@users.noreply.github.com>
2021-10-15 15:41:14 -0700

    Agregado documentacion de JOption

--- Agenda/src/agenda/PruebaAplicacion.java
+++ Agenda/src/agenda/PruebaAplicacion.java
@@ -6,22 +6,78 @@
 package agenda;
 
 import java.util.*;
+import javax.swing.*;
 
 /**
  *
  * @author carlo
  */
-public class PruebaAplicacion {
+
+
+
+    /**
+    * @param args the command line arguments
+    */
+    public class PruebaAplicacion {
     
-   public static void main(String[] args) {
-        Agenda agenda = new Agenda();
-        
+    public static void main(String[] args)
+    {
         PruebaAplicacion pruebApp = new PruebaAplicacion();
+        
+        pruebApp.pruebaJOption();
     }
     
-   public PruebaAplicacion()
-   {
-       System.out.println("penis");
-   }
+    Agenda agenda;
+    
+    public PruebaAplicacion()
+    {
+       agenda = new Agenda();
+    }
+    
+    /*
+        Para mensajes de informacion:
+        JOptionPane.showMessageDialog(null,"Mensaje","Nombre ventana",JOptionPane.INFORMATION_MESSAGE);
+        Para mensajes de error:
+        JOptionPane.showMessageDialog(null,"Mensaje","Nombre ventana",JOptionPane.ERROR_MESSAGE);
+        Para mensajes de aviso importante:
+        JOptionPane.showMessageDialog(null,"Mensaje","Nombre ventana",JOptionPane.WARNING_MESSAGE);
+    
+        Para preguntas si o no y mensajes con confirmacion
+        int respuesta = JOptionPane.showConfirmDialog(null,"Mensaje","Nombre ventana",JOptionPane.YES_NO_OPTION);
+        JOptionPane.YES_NO_OPTION es SI o NO
+        JOptionPane.YES_NO_CANCEL_OPTION es SI, NO o CANCELAR
+        JOptionPane.OK_CANCEL_OPTION es ACEPTAR o CANCELAR
+        JOptionPane.DEFAULT_OPTION es ACEPTAR
+        SI = 0
+        NO = 1
+        CANCEL = 2
+    
+        Para preguntas con salida String
+        String respuesta = JOptionPane.showInputDialog(null,"Mensaje","Text por defecto (Argumento opcional)");
+        JOptionPane.showMessageDialog(null,"Respuesta: "+respuesta,"Nombre ventana",JOptionPane.INFORMATION_MESSAGE);
+        CANCEL = null
+    
+        Para preguntas de seleccion multiple
+        Object[] botones = {1, 2, 3};
+        int respuesta = JOptionPane.showOptionDialog(null,"Mensaje","Nombre ventana",
+        JOptionPane.DEFAULT_OPTION,JOptionPane.QUESTION_MESSAGE,null,botones,botones[0]);
+        Para convertir la respuesta a String:
+        String respuestaString = botones[respuesta].toString();
+        Para convertir la respuesta a Int:
+        int respuestaInt = (Integer)botones[respuesta];
+        
+        
+    */
+    
+    public void pruebaJOption()
+    {
+        Object[] lista = {1, 2, 3};
+        Object respuesta = JOptionPane.showInputDialog(null,"Mensaje","Nombre ventana",
+        JOptionPane.INFORMATION_MESSAGE,null,lista,lista[0]);
+        if(respuesta == null)
+            respuesta = lista[0];
+        int respuestaInt = (Integer)respuesta;
+        JOptionPane.showMessageDialog(null,"Respuesta: "+respuestaInt,"Nombre ventana",JOptionPane.INFORMATION_MESSAGE);
+    }
     
 }
